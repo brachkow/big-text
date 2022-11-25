db4f21ee1c6126959e2c057b5d1422f6e8d272c4
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2022-11-25 14:58:05 +0100

    router added

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import VHomeView from '@/views/VHomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{ path: '/', name: 'home', component: VHomeView }],
 });
 
 export default router;
