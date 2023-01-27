caa00ae1ab32d6ca46c29fe0e35de41138dfb4ad
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-01-27 16:03:39 +0100

    analytics added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { inject } from '@vercel/analytics';
 
 import App from './App.vue';
 import router from './router';
@@ -7,6 +8,8 @@ import router from './router';
 import './tailwind.css';
 
 const app = createApp(App);
+
+inject();
 
 app.use(createPinia());
 app.use(router);
